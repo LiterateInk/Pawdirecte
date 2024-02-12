ea1bb27716a80012627a0d0dbdd6cf7501855808
Mikkel RINGAUD <mikkel@milescode.dev>
2024-02-12 13:47:52 +0100

    docs: add more to the student login example

--- examples/student-login-credentials.ts
+++ examples/student-login-credentials.ts
@@ -5,9 +5,12 @@ import { ExampleCredentialsError, credentials } from "./_credentials";
   // Check the credentials.
   if (!credentials.student_username || !credentials.student_password) throw new ExampleCredentialsError("student");
 
-  const student = await initWithCredentials(credentials.student_username, credentials.student_password);
+  const accounts = await initWithCredentials(credentials.student_username, credentials.student_password);
+  const student = accounts[0]; // We get the first account retrieved.
+
   // We can check the instance of the returned client to be sure we have a student account.
   if (!(student instanceof EDStudent)) throw new Error("Credentials given are not for a student.");
 
-  console.log("OK ! We are logged in.");
+  console.log("Logged in to", student.firstName, student.lastName, "from", student.schoolName);
+  console.log("Token currently used:", student.token);
 })();
