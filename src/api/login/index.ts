91094129ed0391e01e2b65dc75737ab923b8367e
Mikkel RINGAUD <mikkel@milescode.dev>
2024-02-12 08:48:51 +0100

    feat: add login APIhandler

---
+++ src/api/login/index.ts
@@ -0,0 +1,25 @@
+import type { EdApiLogin, EdLogin } from "./types";
+
+import { makeApiHandler } from "~/utils/api";
+import encoder from "~/utils/url-data-encoder";
+
+import { API_URL, API_VERSION } from "~/utils/constants";
+
+export const edApiLogin = makeApiHandler<EdLogin>(async (fetcher, input) => {
+  const data = encoder<EdApiLogin["request"]>({
+    identifiant: input.username,
+    motdepasse: input.password,
+    isReLogin: false,
+    uuid: ""
+  });
+
+  const response = await fetcher(`${API_URL}/login.awp?v=${API_VERSION}`, {
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    method: "POST",
+    body: data
+  });
+
+  const json = await response.json<EdApiLogin["response"]>();
+  if (json.code !== 200) throw new Error(json.message);
+  return json;
+});
