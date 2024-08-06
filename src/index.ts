4ca51625523fff05fdfe8a69fb9d48ff07025f2a
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-08-06 04:26:00 +0200

    chore: rewrite exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-export { default as EDStudent } from "~/clients/Student";
-export { default as EDClientsManager, type EDClientsManagerExported } from "~/clients/Manager";
-export { initWithCredentials, initWithExportedData, type EDClient } from "~/clients";
-export { defaultEDFetcher, type EDFetcher } from "~/utils/fetcher";
-export { default as TimelineItem } from "~/parsers/TimelineItem";
+export * from "./api";
+export * from "./models";
