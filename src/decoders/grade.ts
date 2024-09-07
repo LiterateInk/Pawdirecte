d78ca6c8895a806d1cf6b16d72d981192463f3ee
camarm-dev <contact@camarm.dev>
2024-09-07 21:11:23 +0200

    feat: added decoder for grades

---
+++ src/decoders/grade.ts
@@ -0,0 +1,27 @@
+import { Grade } from "~/models";
+import { decodeGradeValue } from "~/decoders/grade-value";
+
+export const decodeGrade = (item: any): Grade => {
+  return {
+    comment: item.devoir,
+    average: decodeGradeValue(item.moyenneClasse),
+    isOptional: item.valeurisee,
+    skills: [],
+    coefficient: Number(item.coef),
+    date: new Date(item.date),
+    examType: item.typeDevoir,
+    max: decodeGradeValue(item.maxClasse),
+    min: decodeGradeValue(item.minClasse),
+    outOf: item.noteSur,
+    period: {
+      id: item.codePeriode,
+      name: ""
+    },
+    subject: {
+      id: item.codeMatiere,
+      subSubjectId: item.codeSousMatiere,
+      name: item.libelleMatiere
+    },
+    value: decodeGradeValue(item.valeur)
+  };
+};
