9aae6c3330ce7ad06bf5106da16debad92f1fa70
camarm-dev <contact@camarm.dev>
2024-09-07 21:14:43 +0200

    feat: added example for student grades

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,7 @@
 export * from "./account/edforms";
 
 export * from "./student/attendance";
+export * from "./student/grades";
 export * from "./student/homepageTimeline";
 export * from "./student/timeline";
 export * from "./student/timetable";
