afb49d3497b930cd487f91cb6b4986ca9a734085
camarm-dev <contact@camarm.dev>
2024-09-07 21:11:43 +0200

    feat: added POC for student grades

---
+++ src/api/student/grades.ts
@@ -0,0 +1,24 @@
+import { type Account, Grade, type Session, SessionTokenRequired } from "~/models";
+import { Request } from "~/core/request";
+import { decodeGrade } from "~/decoders/grade";
+
+/**
+ * @param year "The year to fetch grades in YYYY format."
+ */
+export const studentGrades = async (session: Session, account: Account, year: string): Promise<Array<Grade>> => {
+  if (!session.token)
+    throw new SessionTokenRequired();
+
+  const request = new Request(`/eleves/${account.id}/notes.awp?verbe=get`)
+    .addVersionURL()
+    .setToken(session.token)
+    .setFormData({
+      anneeScolaire: year
+    });
+
+  const response = await request.send(session.fetcher);
+  session.token = response.token;
+
+  // TODO: a decoder for this, when we know what does it return.
+  return response.data.notes.map(decodeGrade);
+};
