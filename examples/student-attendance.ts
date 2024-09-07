9f58d174d37d81cfb9d438b3a6cc2e721a685b09
camarm-dev <contact@camarm.dev>
2024-09-07 19:37:44 +0200

    feat: added example for attendance

---
+++ examples/student-attendance.ts
@@ -0,0 +1,13 @@
+import { studentAttendance } from "../src";
+
+import { ExampleCredentialsError, credentials } from "./_credentials";
+import { loginUsingCredentials } from "./_login-using-crendentials";
+
+void async function main () {
+  if (!credentials.student_username || !credentials.student_password)
+    throw new ExampleCredentialsError("student");
+
+  const { session, account } = await loginUsingCredentials(credentials.student_username, credentials.student_password);
+  const attendance = await studentAttendance(session, account);
+  console.log(attendance);
+}();
