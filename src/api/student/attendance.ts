e1e51d582097b333e14ab9027a0b4f6472afb53b
camarm-dev <contact@camarm.dev>
2024-09-07 20:01:56 +0200

    fix: code styles

--- src/api/student/attendance.ts
+++ src/api/student/attendance.ts
@@ -1,31 +1,31 @@
 import {type Session, type Account, SessionTokenRequired, type AttendanceItem} from "~/models";
-import { decodeAttendanceItem } from "~/decoders/attendance-item";
-import { Request } from "~/core/request";
+import {decodeAttendanceItem} from "~/decoders/attendance-item";
+import {Request} from "~/core/request";
 
 type AttendanceResponse = {
-    punishments: Array<AttendanceItem>
-    absences: Array<AttendanceItem>
-}
+  punishments: Array<AttendanceItem>
+  absences: Array<AttendanceItem>
+};
 
 export const studentAttendance = async (
-    session: Session,
-    account: Account
+  session: Session,
+  account: Account
 ): Promise<AttendanceResponse> => {
-    if (!session.token)
-        throw new SessionTokenRequired();
+  if (!session.token)
+    throw new SessionTokenRequired();
 
-    const request = new Request(`/eleves/${account.id}/viescolaire.awp?verbe=get`)
-        .addVersionURL()
-        .setToken(session.token);
+  const request = new Request(`/eleves/${account.id}/viescolaire.awp?verbe=get`)
+    .addVersionURL()
+    .setToken(session.token);
 
-    const response = await request.send(session.fetcher);
-    session.token = response.token;
+  const response = await request.send(session.fetcher);
+  session.token = response.token;
 
-    const punishments = response.data.sanctionsEncouragements;
-    const absences = response.data.absencesRetards;
+  const punishments = response.data.sanctionsEncouragements;
+  const absences = response.data.absencesRetards;
 
-    return {
-        punishments: punishments.map(decodeAttendanceItem),
-        absences: absences.map(decodeAttendanceItem)
-    };
+  return {
+    punishments: punishments.map(decodeAttendanceItem),
+    absences: absences.map(decodeAttendanceItem)
+  };
 };
