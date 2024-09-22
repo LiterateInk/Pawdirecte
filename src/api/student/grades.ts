bb471cc9118afef3aaebed52272f83437e4f9c38
camarm-dev <contact@camarm.dev>
2024-09-22 21:45:13 +0200

    feat: restructured grades response

--- src/api/student/grades.ts
+++ src/api/student/grades.ts
@@ -1,4 +1,5 @@
-import { type Account, Grade, Period, type Session, SessionTokenRequired } from "~/models";
+import { type Account, Grade, Period, type Session, SessionTokenRequired, GradeValue } from "~/models";
+import { Response } from "~/core/response";
 import { Request } from "~/core/request";
 import { decodeGrade } from "~/decoders/grade";
 import { decodePeriod } from "~/decoders/period";
@@ -6,7 +7,31 @@ import { decodePeriod } from "~/decoders/period";
 type GradesResponse = {
   grades: Array<Grade>
   periods: Array<Period>
+  overview: {
+    [key: string]: {
+      classAverage: number
+      overallAverage: number
+      subjects: {
+        name: string
+        color: string
+        classAverage: GradeValue
+        maxAverage: GradeValue
+        minAverage: GradeValue
+        studentAverage: GradeValue
+        outOf: GradeValue
+      }[]
+    }
+  }
 };
+
+const buildOverview = (data: any): GradesResponse["overview"] => {
+  const overview = {}
+  for (const period of data.periodes) {
+    // TODO
+  }
+  // TODO
+  return {}
+}
 
 /**
  * @param year "The year to fetch grades in YYYY format." SENSITIVE PARAMATER NOT ALL ACCOUNTS CAN DO THAT
@@ -28,6 +53,7 @@ export const studentGrades = async (session: Session, account: Account, year: st
   // TODO: return parameters like colors ect...
   return {
     grades: response.data.notes?.map(decodeGrade),
-    periods: response.data.periodes?.map(decodePeriod)
+    periods: response.data.periodes?.map(decodePeriod),
+    overview: buildOverview(response.data)
   };
 };
