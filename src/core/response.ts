77e5b0d10732d7ea06e51d5113f757b50c54d956
camarm-dev <contact@camarm.dev>
2024-09-28 22:14:12 +0200

    fix(response): checking if returned value can be transformed to JSON before setting request as errored (thanks to @imyanice)
    Co-authored-by: Yanice <git@yanice.me>

--- src/core/response.ts
+++ src/core/response.ts
@@ -1,5 +1,9 @@
 import { getHeaderFromResponse, type Response as UnsafeResponse } from "@literate.ink/utilities";
 
+const validJson = (value: string) => {
+  return (value.startsWith("[") || value.startsWith("{")) && (value.endsWith("]") || value.endsWith("}"))
+}
+
 export class Response {
   public status: number;
   public token: string | null;
@@ -11,7 +15,8 @@ export class Response {
     this.token = getHeaderFromResponse(response, "x-token");
 
     const content_type = getHeaderFromResponse(response, "content-type");
-    if (!content_type?.startsWith("application/json")) {
+    // Set error if response is not JSON and don't starts like JSON. ED sometimes return JSON in a text/html Content-Type (yes....)
+    if (!content_type?.startsWith("application/json") && !validJson(response.content)) {
       this.status = parseInt(getHeaderFromResponse(response, "x-code")!, 10);
     }
     else {
