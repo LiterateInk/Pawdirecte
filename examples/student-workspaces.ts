8014ea108ccd4fd6f44031d50f259159608cb40c
camarm-dev <contact@camarm.dev>
2024-09-28 12:51:53 +0200

    fix: added test for workspaces

---
+++ examples/student-workspaces.ts
@@ -0,0 +1,13 @@
+import { studentWorkspace } from "../src";
+
+import { ExampleCredentialsError, credentials } from "./_credentials";
+import { loginUsingCredentials } from "./_login-using-crendentials";
+
+void async function main () {
+  if (!credentials.student_username || !credentials.student_password)
+    throw new ExampleCredentialsError("student");
+
+  const { session, account } = await loginUsingCredentials(credentials.student_username, credentials.student_password);
+  const workspaces = await studentWorkspace(session, account);
+  console.log(workspaces);
+}();
