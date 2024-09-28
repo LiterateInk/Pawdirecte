6531a4ff1c63dfdda909db591756e40a62c60bbb
camarm-dev <contact@camarm.dev>
2024-09-28 20:26:46 +0200

    fix: reverted document kind types

--- src/decoders/document-kind.ts
+++ src/decoders/document-kind.ts
@@ -2,10 +2,12 @@ import { UnknownEnumValue, DocumentKind } from "~/models";
 
 export const decodeDocumentKind = (kind: any): DocumentKind => {
   switch (kind) {
-    case DocumentKind.Attachment: return DocumentKind.Attachment;
-    case DocumentKind.CDT: return DocumentKind.CDT;
-    case DocumentKind.ClickNCollect: return DocumentKind.ClickNCollect;
-    case DocumentKind.Cloud: return DocumentKind.Cloud;
-    default: throw new UnknownEnumValue("AccountKind", kind);
+    case DocumentKind.Document: return DocumentKind.Document;
+    case DocumentKind.Grades: return DocumentKind.Grades;
+    case DocumentKind.SchoolLife: return DocumentKind.SchoolLife;
+    case DocumentKind.Invoice: return DocumentKind.Invoice;
+    case DocumentKind.Registration: return DocumentKind.Registration;
+    case DocumentKind.Other: return DocumentKind.Other;
+    default: throw new UnknownEnumValue("DocumentKind", kind);
   }
 };
--- src/models/document-kind.ts
+++ src/models/document-kind.ts
@@ -1,8 +1,10 @@
 export const DocumentKind = {
-  Cloud: "Cloud",
-  CDT: "FICHIER_CDT",
-  Attachment: "PIECE_JOINTE",
-  ClickNCollect: "FICHIER_MENU_RESTAURATION"
+  Grades: "Note",
+  Document: "Doc",
+  SchoolLife: "Viesco", // TODO, not sure
+  Invoice: "Fac", // TODO, not sure
+  Registration: "Inscr", // TODO, not sure
+  Other: ""
 } as const;
 
 export type DocumentKind = typeof DocumentKind[keyof typeof DocumentKind];
