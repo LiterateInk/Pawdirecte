dedd25f456bea9edcec661494efeb2c2682ac84b
camarm-dev <contact@camarm.dev>
2024-09-28 23:54:50 +0200

    fix: resolved https://github.com/LiterateInk/Pawdirecte/pull/4/files/528396677a322d7ce5cf0448a8f4faa5bdba8028#r1779769216

--- src/decoders/document-kind.ts
+++ src/decoders/document-kind.ts
@@ -1,14 +1,5 @@
-import { UnknownEnumValue, DocumentKind } from "~/models";
+import { DocumentKind } from "~/models";
 
 export const decodeDocumentKind = (kind: any): DocumentKind => {
-  switch (kind) {
-    case DocumentKind.Document: return DocumentKind.Document;
-    case DocumentKind.Grades: return DocumentKind.Grades;
-    case DocumentKind.SchoolLife: return DocumentKind.SchoolLife;
-    case DocumentKind.Invoice: return DocumentKind.Invoice;
-    case DocumentKind.Registration: return DocumentKind.Registration;
-    case DocumentKind.Textbook: return DocumentKind.Textbook;
-    case DocumentKind.Other: return DocumentKind.Other;
-    default: throw new UnknownEnumValue("DocumentKind", kind);
-  }
+  return kind as DocumentKind;
 };
